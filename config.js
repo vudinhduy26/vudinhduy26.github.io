188c04e20a2baeeb4d2608056d80b0beb51d4d41
vudinhduy26 <pikaduy26@gmail.com>
2022-07-11 13:54:51 +0700

    up

---
+++ config.js
@@ -0,0 +1,29 @@
+var config = {
+    texts: [
+        "Em À !",  
+        "Em biết không, Em đặc biệt lắm", 
+        "Thỉnh thoảng lại gì đâu à hmmmm",
+        "Đó là nét riêng của em , Anh yêu cả điều đó ạ ^.^",
+        "Chỉ là mỗi lần như thế em đừng bỏ anh đi như những cuộc tình trc được hông :((",
+        "Anh vẫn chờ em , bé iu của anh về với anh !!!",
+        "Qua mỗi lần em chia sẻ về nyc anh đề trân trọng và rút kinh nghiệm cho bản thân mình",
+        "Để em an nhiên vững tâm vào anh <3 !",
+        "Còn nhiều điều muốn nói lắm í !!!",
+        "Anh muốn nói thêm là ...",
+        "ANH YÊU EM, BÙI YẾN LINH <3 'xin pass FB ii :3'",
+    ],
+
+    imgs: {
+        "Em À !": "./assets/duy.jpg",
+    },
+    desc: {
+        turn_on: "Tối Cá",
+        play: "Thêm chút nhạc ạ ^^",
+        bannar_coming: "Bất Ngờ cho em :3",
+        balloons_flying: "Hihi",
+        cake_fadein: "Linh Xinh Gái Hihi",
+        light_candle: "Linh Xinh Gái Hihi",
+        wish_message: "WoW",
+        story: "Anh muốn nói với em là :3",
+    }
+};
